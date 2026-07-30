0622bee0eea1d37f4e37efa20d2c2ee82d1fda3b
DanilSJ <danilchagarnoy@mail.ru>
2026-07-30 23:07:12 +0300

    Create modul parse

--- bot/parse/parse.py
+++ bot/parse/parse.py
@@ -1,6 +1,7 @@
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from core.config import bot
+import bot.services.parsing as parsing_service
 import asyncio
 
 
@@ -9,8 +10,6 @@ async def start_parse_task(message: Message, state: FSMContext):
     if data.get("parser_task") and not data["parser_task"].done():
         await message.answer("Парсинг уже запущен!")
     else:
-        import bot.services.parsing as parsing_service
-
         task = asyncio.create_task(
             parsing_service.parse_projects_and_send(bot, message.chat.id, state)
         )
