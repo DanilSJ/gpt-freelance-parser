008cc2d24f89cd6682404936ffaa8d1b2108f5a8
DanilSJ <danilchagarnoy@mail.ru>
2026-07-30 22:56:28 +0300

    The old user-agents have been removed and new unique constantly generated user-agents have been added

--- app/header.py
+++ app/header.py
@@ -1,13 +1,13 @@
-from core.user_agents import user_agents
-import random
+from fake_useragent import UserAgent
 
+ua = UserAgent()
 
 def build_headers(referer: str = None) -> dict:
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
         "Connection": "keep-alive",
-        "User-Agent": random.choice(user_agents),
+        "User-Agent": print(ua.random),
         "Referer": referer if referer else "https://freelance.ru/",
     }
     return headers
